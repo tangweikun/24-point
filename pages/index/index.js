9457922f771fb6973288198244e964dab1bcfa2b
tangweikun <819573105@qq.com>
2018-01-11 23:34:45 +0800

    :hammer: Refactory generateCards

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,9 @@
-//index.js
-//获取应用实例
 const app = getApp()
 const util = require('../../utils/util')
+
+const randomNumber = () => Math.ceil(Math.random() * 13)
+const generateCards = () =>
+  [0, 1, 2, 3].map(() => ({ value: randomNumber(), isDisabled: false }))
 
 Page({
   data: {
@@ -11,12 +13,7 @@ Page({
     operators: ['+', '-', '*', '/'],
     currentOperator: null,
     currentCard: null,
-    cards: [
-      { value: Math.ceil(Math.random() * 13), isDisabled: false },
-      { value: Math.ceil(Math.random() * 13), isDisabled: false },
-      { value: Math.ceil(Math.random() * 13), isDisabled: false },
-      { value: Math.ceil(Math.random() * 13), isDisabled: false },
-    ],
+    cards: generateCards(),
   },
   //事件处理函数
   bindViewTap: function() {
@@ -24,49 +21,32 @@ Page({
       url: '../logs/logs',
     })
   },
+
   onLoad: function() {
     if (app.globalData.userInfo) {
-      const randomNumber = () => Math.ceil(Math.random() * 13)
-      const generateCards = () => [
-        { value: randomNumber(), isDisabled: false },
-        { value: randomNumber(), isDisabled: false },
-        { value: randomNumber(), isDisabled: false },
-        { value: randomNumber(), isDisabled: false },
-      ]
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-        cards: generateCards(),
-      })
+      this.setData({ userInfo: app.globalData.userInfo, hasUserInfo: true })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-        })
+        this.setData({ userInfo: res.userInfo, hasUserInfo: true })
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-          })
+          this.setData({ userInfo: res.userInfo, hasUserInfo: true })
         },
       })
     }
   },
+
   getUserInfo: function(e) {
     app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true,
-    })
+    this.setData({ userInfo: e.detail.userInfo, hasUserInfo: true })
   },
+
   selectCardOrOperator: function(e) {
     const { type, value, index } = e.currentTarget.dataset
     const { cards, currentOperator, currentCard } = this.data
@@ -104,12 +84,7 @@ Page({
 
   reset: function(e) {
     this.setData({
-      cards: [
-        { value: Math.ceil(Math.random() * 13), isDisabled: false },
-        { value: Math.ceil(Math.random() * 13), isDisabled: false },
-        { value: Math.ceil(Math.random() * 13), isDisabled: false },
-        { value: Math.ceil(Math.random() * 13), isDisabled: false },
-      ],
+      cards: generateCards(),
       currentCard: null,
       currentOperator: null,
     })
