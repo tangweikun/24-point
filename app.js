d83c69171d1e66c50e0abc405afc6f71bd5a5c7f
tangweikun <819573105@qq.com>
2018-06-22 16:52:01 +0800

    :sparkles: Record user operation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -8,9 +8,39 @@ App({
 
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+      success: res1 => {
+        wx.getSetting({
+          success: res2 => {
+            if (res2.authSetting['scope.userInfo']) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+              wx.getUserInfo({
+                success: res2 => {
+                  wx.request({
+                    url: 'https://api.tangweikun.cn/createuser',
+                    method: 'post',
+                    data: {
+                      code: res1.code,
+                      userInfo: res2.userInfo,
+                    },
+                    success: response => {
+                      this.globalData.userInfo = res2.userInfo
+                      this.globalData.openid = response.data.openid
+                    },
+                  })
+                  // 可以将 res2 发送给后台解码出 unionId
+                  // this.globalData.userInfo = res2.userInfo
+
+                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                  // 所以此处加入 callback 以防止这种情况
+                  if (this.userInfoReadyCallback) {
+                    this.userInfoReadyCallback(res2)
+                  }
+                },
+              })
+            }
+          },
+        })
+      },
     })
     // 获取用户信息
     wx.getSetting({
@@ -21,19 +51,20 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
+              console.log('---->>', res)
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
-            }
+            },
           })
         }
-      }
+      },
     })
   },
   globalData: {
-    userInfo: null
-  }
+    userInfo: null,
+  },
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -29,19 +29,28 @@ Page({
 
   onLoad: function() {
     if (app.globalData.userInfo) {
-      this.setData({ userInfo: app.globalData.userInfo, hasUserInfo: true })
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+      })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({ userInfo: res.userInfo, hasUserInfo: true })
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        })
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
-          this.setData({ userInfo: res.userInfo, hasUserInfo: true })
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+          })
         },
       })
     }
@@ -62,7 +71,6 @@ Page({
   selectCard: function(e) {
     const { value, index } = e.currentTarget.dataset
     const { cards, currentOperator, currentCard } = this.data
-
     if (cards[index].state === 'disable') return
 
     const nextState = {
@@ -109,6 +117,21 @@ Page({
     }
     const isFinish =
       nextState.cards.filter(({ state }) => state === 'disable').length === 3
+
+    const openid = app.globalData.openid
+    if (isFinish && openid !== '') {
+      wx.request({
+        url: 'https://api.tangweikun.cn/increaseAnswersCount',
+        method: 'post',
+        data: {
+          openid,
+          isCorrect: nextState.currentCard.value === 24,
+        },
+        success: res => {
+          console.log(res, '999')
+        },
+      })
+    }
 
     this.setData({ ...nextState, isFinish })
   },
