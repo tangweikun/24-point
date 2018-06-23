a1d72accd5d0c68fc5b2f279432b1909341d45a0
tangweikun <819573105@qq.com>
2018-06-23 18:56:47 +0800

    :fire: Remove useless code

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,9 +13,6 @@ const defaultCards = generateCards()
 
 Page({
   data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     operators: ['+', '-', '*', '/'],
     currentOperator: null,
     currentCard: null,
@@ -25,6 +22,7 @@ Page({
     totalOfAnswers: 0,
     totalOfCorrectAnswers: 0,
   },
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -32,39 +30,7 @@ Page({
     })
   },
 
-  onLoad: function() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-          })
-        },
-      })
-    }
-  },
-
-  getUserInfo: function(e) {
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({ userInfo: e.detail.userInfo, hasUserInfo: true })
-  },
+  onLoad: function() {},
 
   selectOperator: function(e) {
     const { value } = e.currentTarget.dataset
@@ -74,7 +40,6 @@ Page({
   },
 
   selectCard: function(e) {
-    console.log(this.data, '--')
     const { value, index } = e.currentTarget.dataset
     const { cards, currentOperator, currentCard } = this.data
     if (cards[index].state === 'disable') return
