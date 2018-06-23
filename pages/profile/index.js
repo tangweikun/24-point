eb5a74aff04b31b43f2d551d786f43e4a54ed590
tangweikun <819573105@qq.com>
2018-06-24 01:23:24 +0800

    :sparkles: Add userInfo

---
+++ pages/profile/index.js
@@ -0,0 +1,64 @@
+const app = getApp()
+
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isAuthorized: false,
+    totalOfAnswers: 0,
+    totalOfCorrectAnswers: 0,
+    ranking: 0,
+    accuracy: 100,
+  },
+
+  onLoad: function() {},
+
+  onShow: function() {
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.request({
+            url: 'https://api.tangweikun.cn/getUserInfo',
+            method: 'post',
+            data: {
+              openid: app.globalData.openid,
+            },
+            success: response => {
+              const {
+                userInfo,
+                totalOfCorrectAnswers = '-',
+                totalOfAnswers = '-',
+                ranking = '-',
+              } = response.data
+
+              app.globalData.userInfo = userInfo
+              this.setData({
+                totalOfCorrectAnswers,
+                totalOfAnswers,
+                ranking,
+                isAuthorized: true,
+                accuracy:
+                  ((100 * totalOfCorrectAnswers) / totalOfAnswers).toFixed(2) +
+                  '%',
+              })
+            },
+          })
+        }
+      },
+    })
+  },
+
+  bindGetUserInfo: function(e) {
+    wx.request({
+      url: 'https://api.tangweikun.cn/updateUserInfo',
+      method: 'post',
+      data: {
+        openid: app.globalData.openid,
+        userInfo: e.detail.userInfo,
+      },
+      success: response => {
+        console.log(response)
+      },
+    })
+    this.setData({ isAuthorized: true })
+  },
+})
