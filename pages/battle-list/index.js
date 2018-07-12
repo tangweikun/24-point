1f47bd8b539d66d5c15fdc34cd111178cec9a346
tangweikun <819573105@qq.com>
2018-07-12 10:43:36 +0800

    :lipstick: Update UI of BattleList

--- pages/battle-list/index.js
+++ pages/battle-list/index.js
@@ -1,4 +1,6 @@
 const app = getApp()
+const { BASE_URL } = require('../../constants/index.js')
+const { formatTime } = require('../../utils/index.js')
 
 Page({
   data: {
@@ -7,6 +9,20 @@ Page({
 
   onLoad: function() {
     this.setData({ battleList: app.globalData.battleList })
+    const { openid } = app.globalData
+    if (openid) {
+      wx.request({
+        url: `${BASE_URL}/getMyBattleList`,
+        method: 'post',
+        data: { openid },
+        success: res => {
+          app.globalData.battleList = res.data.map(x => ({
+            ...x,
+            createdAt: formatTime(x.createdAt),
+          }))
+        },
+      })
+    }
   },
 
   onShareAppMessage: function(res) {
