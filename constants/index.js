5bd12fad58695d0c79f4c04dcca4a54051004a43
tangweikun <819573105@qq.com>
2019-05-07 16:37:42 +0800

    :alien: Update server url

--- constants/index.js
+++ constants/index.js
@@ -11,5 +11,5 @@ module.exports = {
   RULE:
     '玩家得到4个1~13之间的数字，运用这个4个数字进行加减乘除四则运算来算出24',
   // BASE_URL: 'http://localhost:4000',
-  BASE_URL: 'https://api.tangweikun.cn',
+  BASE_URL: 'https://api-ghost.herokuapp.com',
 };
